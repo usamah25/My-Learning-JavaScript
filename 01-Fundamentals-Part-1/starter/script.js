279d2b6f11fc66c23fa931d52a06fcee1772f2ba
Usamah Abdillah Hanif <usamahhanif3@gmail.com>
2024-01-25 10:13:28 +0700

    1.1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,37 @@
+/* 
 let js = 'Amazing';
-if (js === 'Amazing') alert('JavaScript Is Fun');
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Usamah");
+console.log(23);
+
+let firstName = "Usamah";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Student"; 
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Usamah');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 2022;
+console.log(typeof year);
