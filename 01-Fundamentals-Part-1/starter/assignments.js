927e03a40ff1987cda810fe3d413779075edc3cd
Usamah Abdillah Hanif <usamahhanif3@gmail.com>
2024-01-26 17:13:09 +0700

    1.0.5

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+let country = 'Indonesian';
+const continent = 'Asian';
+let population = 200;
+let isIsland = true;
+let language = 'Indonesian'; 
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
