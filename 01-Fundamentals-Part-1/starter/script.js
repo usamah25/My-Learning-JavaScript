f22390739a8a538f427c0b29a932c8deb4a9aaf5
Usamah Abdillah Hanif <usamahhanif3@gmail.com>
2024-01-26 17:12:24 +0700

    1.0.5

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -80,7 +80,7 @@ const isFullAge = ageSarah >= 18;
 
 console.log(now - ageUsamah > now - ageSarah);
 */
-
+/* 
 // type conversion
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
@@ -95,4 +95,47 @@ console.log(String(23), 23);
 console.log("I'am" + 23 + 'years old' );
 console.log('23' - '10' - 3);
 console.log('23' * '2');
-console.log('23' / '2');
+console.log('23' / '2'); 
+*/
+
+const day = 'thursday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure')
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+    default:
+        console.log('Not a valid day!');
+        break;
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure')
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thurday') {
+    console.log('Write code examples');
+} else if (day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
