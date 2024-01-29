943218684d3ae2182d582dd1344be23af1c8aab8
Usamah Abdillah Hanif <usamahhanif3@gmail.com>
2024-01-29 18:05:54 +0700

    fixex

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -10,3 +10,30 @@ const saudiArabian = describeCountry('Arab Saudi', 10, 'Riyadh');
 console.log(indonesian);
 console.log(finland);
 console.log(saudiArabian);
+
+function percentageWorld1(country, population) {
+    let result = (population / 7900) * 100;
+    return `${country} has ${population} million people, so it's about ${result}% of the world population`
+}
+
+const percentageIndonesian = percentageWorld1('Indonesian', 200);
+console.log(percentageIndonesian);
+
+const percentageWorld2 = function (country, population) {
+    let result = (population / 7900) * 100;
+    return `${country} has ${population} million people, so it's about ${result}% of the world population`
+}
+
+const percentageFinland = percentageWorld2('Finland', 6);
+console.log(percentageFinland);
+
+const percentageChina = percentageWorld2('China', 1441);
+console.log(percentageChina)
+
+const percentageWorld3 = (country, population) => {
+    let result = (population / 7900) * 100;
+    return `${country} has ${population} million people, so it's about ${result}% of the world population`
+}
+
+const percentageSaudiArabian = percentageWorld3('Saudi Arabia', 10);
+console.log(percentageSaudiArabian)
