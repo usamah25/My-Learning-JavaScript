cc1c446f2cee60e92e38798638abb7fa17c8e6e1
Usamah Abdillah Hanif <usamahhanif3@gmail.com>
2024-01-31 21:25:49 +0700

    1.0.6

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,5 +1,6 @@
 'use strict';
-function describeCountry(country, population, capitalCity) {
+
+/* function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and its capital city is ${capitalCity}`
 }
 
@@ -36,4 +37,80 @@ const percentageWorld3 = (country, population) => {
 }
 
 const percentageSaudiArabian = percentageWorld3('Saudi Arabia', 10);
-console.log(percentageSaudiArabian)
+console.log(percentageSaudiArabian) 
+*/
+
+/* const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey)
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(`Jonas has 3 friends, and his best friend is called ${jonas.friends[0]}`);
+ */
+
+/* 
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function() {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+    getSummary: function() {
+        return `${this.firstName} is ${this.calcAge()} a-year old ${this.job}, and ${(this.hasDriversLicense ? 'he has a drivers license' : 'he has no drivers license' )}`
+    }
+};
+console.log(getSummary()); 
+ */
+
+const populations = [10, 1441, 332, 83];
+
+console.log(populations.length === 4);
+
+const percentages = [
+    percentageOfWorld1(populations[0]),
+    percentageOfWorld1(populations[1]),
+    percentageOfWorld1(populations[2]),
+    percentageOfWorld1(populations[3])
+];
+
+console.log(percentages);
+
+let percentages2 = [];
+
+for(let i = 0; i < populations.length; i++) {
+    percentages2.push(percentageOfWorld1(populations[i]));
+}
